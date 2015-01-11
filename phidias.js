c2a82cbe8ce582273d7bc66d586b7adff9773b6c
Andrew Taylor <mac@andrewt.net>
2015-01-11 14:33:26 +0000

    Run on startup

--- phidias.js
+++ phidias.js
@@ -1,10 +1,15 @@
-window.addEventListener('resize', function() {
-	var imgs = document.getElementsByTagName('img');
-	for (var i = 0; i < imgs.length; ++i) {
-		var img = imgs[i]
-			width = img.getAttribute('width'),
-			height = img.getAttribute('height');
-		if (width && height)
-			img.style.height = img.clientWidth * width / height + 'px';
+(function() {
+	document.addEventListener('DOMContentLoaded', resize);
+	window.addEventListener('resize', resize);
+
+	function resize() {
+		var imgs = document.getElementsByTagName('img');
+		for (var i = 0; i < imgs.length; ++i) {
+			var img = imgs[i]
+				width = img.getAttribute('width'),
+				height = img.getAttribute('height');
+			if (width && height)
+				img.style.height = img.clientWidth * width / height + 'px';
+		}
 	}
-});
+})();
