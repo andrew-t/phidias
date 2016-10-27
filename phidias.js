a652de7811879cd3d631c74ee5cd79b87819a63e
Andrew Taylor <Andrew.Taylor04@bbc.co.uk>
2016-10-27 13:40:59 +0100

    Allow explicit flag

--- phidias.js
+++ phidias.js
@@ -3,15 +3,20 @@
 	window.addEventListener('resize', resize);
 
 	function resize() {
-		['img', 'iframe'].forEach(function(tagName) {
-			var elements = document.getElementsByTagName(tagName);
-			for (var i = 0; i < elements.length; ++i) {
-				var element = elements[i],
-					width = element.getAttribute('width'),
-					height = element.getAttribute('height');
-				if (width && height)
-					element.style.height = element.clientWidth * height / width + 'px';
-			}
-		})
+		resizeElements(document.getElementsByClassName('phidias'));
+		resizeElements(document.getElementsByTagName('img'));
+		resizeElements(document.getElementsByTagName('iframe'));
+	}
+
+	function resizeElements(elements) {
+		for (var i = 0; i < elements.length; ++i)
+			resizeElement(elements[i]);
+	}
+
+	function resizeElement(element) {
+		var width = element.getAttribute('width'),
+			height = element.getAttribute('height');
+		if (width && height)
+			element.style.height = element.clientWidth * height / width + 'px';
 	}
 })();
